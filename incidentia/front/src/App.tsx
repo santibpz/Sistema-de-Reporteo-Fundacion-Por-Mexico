cc2b462541f5596cd0957e79a07d9529f26128a7
mtumalan <mau@tumalan.com>
2023-09-25 17:41:08 -0600

    Se añade página de charts al app.tsx

--- incidentia/front/src/App.tsx
+++ incidentia/front/src/App.tsx
@@ -12,32 +12,36 @@ import { i18nProvider } from "./i18nProvider";
 import { dataProvider } from "./dataProvider";
 import authProvider from "./authProvider";
 import LoginPage from "./pages/LoginPage";
-
+import ChartPage from "./pages/ChartPage";
 import Reporte from "./components/Reportes/Reporte";
-
 import { Route } from 'react-router-dom';
-
 import { ReporteShow } from "./components/Reportes/Reporte"
+const CRoutes = [< Route path="reportes/show/:id" Component={ReporteShow} />,
+                 < Route path="chart" Component={ChartPage} />
+                ];
 
 //hola
 export const App = () => {
-  
+
   return(
     <Admin 
       loginPage = {LoginPage} 
       dataProvider={dataProvider} 
       authProvider = {authProvider}
       i18nProvider={i18nProvider}
+      customRoutes={CRoutes}
       >
 
       <Resource 
        name="reportes" 
        list={Reporte.ReporteList} 
        create = {Reporte.ReporteCreate} 
-        />  
-      <CustomRoutes>
-            <Route path="reportes/show/:id" element={<ReporteShow />} />
-      </CustomRoutes>
+      />
+
+      <Resource
+      name="chart"
+      list={ChartPage}
+      />
     </Admin>  
   )
 }
--- incidentia/front/src/dataProvider.ts
+++ incidentia/front/src/dataProvider.ts
@@ -2,4 +2,4 @@ import simpleRestProvider from "ra-data-simple-rest";
 
 export const dataProvider = simpleRestProvider(
   import.meta.env.VITE_SIMPLE_REST_URL
-);
+);  
