2fa487e5686f14b319d91e0d950d43d0ace4a056
MrDrHax <alexfh2001@gmail.com>
2023-10-13 21:32:09 -0600

    creo que ya?

--- install.sh
+++ install.sh
@@ -36,10 +36,13 @@ sudo apt update
 
 # install node
 echo "installing node"
-# run without sudo:
-sudo -u $SUDO_USER curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-sudo -u $SUDO_USER source ~/.bashrc
-sudo -u $SUDO_USER nvm install node
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 # install nginx
 echo "installing nginx"
